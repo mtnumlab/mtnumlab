d06dc91b828e5b3478e93cc6873693e052193e6f
mtnumlab <96823480+mtnumlab@users.noreply.github.com>
2021-12-30 16:25:00 +0530

    Delete bernouli.py

--- bernouli.py
+++
@@ -1,9 +0,0 @@
-import silence_tensorflow.auto
-import tensorflow as tf
-import tensorflow_probability as tfp
-
-# create a model with known probability
-event = tfp.distributions.Bernoulli(probs=0.8)
-sample = event.sample(100)
-sample_mean = tf.reduce_mean(tf.cast(sample, float))
-print(sample_mean)
